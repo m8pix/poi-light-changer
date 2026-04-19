4d319fa78f9f3fef3070cdf1a4c5e5e00fa012f6
m8pix <maya1231@MacBookAir2024.local>
2026-04-19 17:29:33 +0900

    commit

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -41,10 +41,6 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
@@ -87,7 +83,7 @@ const config: Config = {
           position: 'left',
           label: 'Docs',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        {to: '/docs/install', label: 'Install', position: 'left'},
         {
           href: 'https://booth.pm',
           label: 'Booth Store',
